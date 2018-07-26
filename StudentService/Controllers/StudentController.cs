48145ecf3901328b32eb30ecbba31443a3b315fc
patrick.brocato <patrickbrocato@gmail.com>
2018-07-26 09:06:16 -0400

    Basic Functionality
    I have added the basic functionality. You can retrieve student information using an api call.

---
+++ StudentService/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using StudentDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentService.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public IEnumerable<Student> Get()
+        {
+            using (StudentsEntities e = new StudentsEntities())
+            {
+                return e.Students.ToList();
+            }
+        }
+
+        public Student Get(int id)
+        {
+            using (StudentsEntities e = new StudentsEntities())
+            {
+                return e.Students.FirstOrDefault(s => s.StudentID == id);
+            }
+        }
+    }
+}
